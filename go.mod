fb4eb419991c69154e30dfbe5bf5a801f09456a1
Flavio Perri <flavio80@gmail.com>
2024-03-23 13:07:21 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arkthos80/game-of-100
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	fmt.Println("Ready to find a solution to the Game Of 100 solitaire!")
+}
